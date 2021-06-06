ff91a3180dbd38fe3fc77b20b40d23828065325f
John Lovato <jtlovato@gmail.com>
2021-06-06 14:00:21 -0600

    Updated footer, design, code

--- js/behavior.js
+++ js/behavior.js
@@ -1,17 +1,20 @@
 
-var btn = document.getElementById('btn');
-var d = new Date();
-var n = d.getFullYear();
-var footer = document.getElementById('footer');
+let btn = document.getElementById('btn');
+let d = new Date();
+const one_day = 1000 * 60 * 60 * 24 
+const creation = new Date(1986, 6, 19) 
+const Result = Math.round( d.getTime() - creation.getTime()) / (one_day); 
+const Final_Result = Result.toFixed(0); 
+let birthday = (Final_Result); 
+let aboutSpan = document.getElementById("about-span");
 
 function colors() {
+    let st0 = document.getElementsByClassName('st0');
+    let circle = document.getElementById('circle');
+    let bg = document.getElementById('body');
+    let btn = document.getElementById('btn');
 
-    var st0 = document.getElementsByClassName('st0');
-    var circle = document.getElementById('circle');
-    var bg = document.getElementById('body');
-    var btn = document.getElementById('btn');
-
-    for(var i = 0, length = st0.length; i < length; i++) {
+    for(let i = 0, length = st0.length; i < length; i++) {
         st0[i].classList.toggle("dark-mode");
         console.log("Ugh");
         }
@@ -23,55 +26,44 @@ function colors() {
     } else {
         btn.innerHTML = "DARK MODE";
     }
-    }
+}
 
     // Contact Below
 
-    function resubmit() {
-        console.log("sweet");
-    }
+function resubmit() {
+    console.log("sweet");
+}
 
-    window.onload = function() {
-        document.getElementById('contact-form').addEventListener('submit', function(event) {
-            event.preventDefault();
-            this.contact_number.value = Math.random() * 100000 | 0;
-            emailjs.sendForm('service_rlhl0xh', 'template_3s0flsq', this)
-                .then(function() {
-                    var contactInfo = document.getElementById("contact-info");
-                    console.log('SUCCESS!');
-                    contactInfo.innerHTML = "Thanks! We'll get back to you as soon as possible.";
-                }, function(error) {
-                    var contactInfo = document.getElementById("contact-info");
-                    console.log('FAILED...', error);
-                    contactInfo.innerHTML = "Hang on.....something went wrong..somewhere....<br></br>How about this? Just email me directly at jtlovato@gmail.com. Thanks!";
-                });
-        });
-    }
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        this.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm('service_rlhl0xh', 'template_3s0flsq', this)
+            .then(function() {
+                let contactInfo = document.getElementById("contact-info");
+                console.log('SUCCESS!');
+                contactInfo.innerHTML = "Thanks! We'll get back to you as soon as possible.";
+            }, function(error) {
+                let contactInfo = document.getElementById("contact-info");
+                console.log('FAILED...', error);
+                contactInfo.innerHTML = "Hang on.....something went wrong..somewhere....<br></br>How about this? Just email me directly at jtlovato@gmail.com. Thanks!";
+            });
+    });
+}
 
     // About page
 
-    const one_day = 1000 * 60 * 60 * 24 
-    const present_date = new Date(); 
-    const creation = new Date(1986, 5, 19) 
-    const Result = Math.round( present_date.getTime() - creation.getTime()) / (one_day); 
-    const Final_Result = Result.toFixed(0); 
-    let birthday = (Final_Result); 
-    let aboutSpan = document.getElementById("about-span");
-
-    aboutSpan.innerHTML = birthday + " ";
+aboutSpan.innerHTML = birthday + " ";
 
 
     // Code
 
 
-    function flip() {
-        var cube = document.getElementById('cube');  
-        cube.classList.add("cube-rotate")
+function rotate() {
+
+    let cube = document.getElementsByClassName('cube');
+            
+    for(let i = 0, length = cube.length; i < length; i++) {
+        cube[i].classList.toggle("cube-rotate");
     }
-    
-
-    
-
-// Footer
-
-footer.innerHTML = "&copy" + n;
+}

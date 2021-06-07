932f82a4f9528279f15b8739c5b23d503833de09
John Lovato <jtlovato@gmail.com>
2021-06-07 13:56:05 -0600

    Finished Mobile Version, Adding Alt Tags

--- js/behavior.js
+++ js/behavior.js
@@ -30,10 +30,6 @@ function colors() {
 
     // Contact Below
 
-function resubmit() {
-    console.log("sweet");
-}
-
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();

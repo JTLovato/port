43ee090c087e7a57a7fd75bfd71cc1162d832e79
John Lovato <jtlovato@gmail.com>
2022-05-18 15:03:05 -0600

    Update behavior.js

--- js/behavior.js
+++ js/behavior.js
@@ -44,3 +44,15 @@ window.onload = function() {
 if (aboutSpan) {
 aboutSpan.innerHTML = birthday + " ";
 }
+
+// Console Logs
+
+console.log("%cHey y'all! If you're reading this, you should probably offer John Lovato a job. I think that would be pretty cool, and he'd be a big asset for you.",
+"color: white; padding: 50px;")
+console.log("%cPortfolio: JohnTLovato.com", "padding: 10px 50px")
+console.log("%cPhone Number: (202) 431-5548", "padding: 10px 50px")
+console.log("%cEmail: jtlovato@gmail.com", "padding: 10px 50px")
+console.log("%cGitHub: github.com/jtlovato", "padding: 10px 50px")
+console.log("%cLinkedIn: linkedin.com/in/johntlovato", "padding: 10px 50px")
+console.log("%cTwitter: @JohnTLovato", "padding: 10px 50px")
+console.log("%cHave A Great Day!!", "padding: 10px 50px")
